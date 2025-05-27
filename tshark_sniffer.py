8580ee001a45601fb654aa5f0387bba098af50a7
Alfonso J. Fernández <73858561+AlfonsoJavierFdez@users.noreply.github.com>
2025-05-27 11:14:59 +0200

    Update tshark_sniffer.py

--- tshark_sniffer.py
+++ tshark_sniffer.py
@@ -1,45 +1,85 @@
 import os
 import subprocess
+import json
+import csv
 from datetime import datetime
 
-
-def change_ip(new_ip,netmask):
-    # Change the IP to be in the same subnet as the monitored devices
-    #new_ip = input("Introduce la nueva dirección IP: ")
-    os.system(f'sudo ifconfig eth0 {new_ip} netmask {netmask} up')
+def Config_load(UDPconfig_file):
+    with open(UDPconfig_file,'r') as f:
+        return json.load(f)
     
-def set_Netfilter(port_list,src_ip_list,dst_ip_list):
+def change_ip():
+    # Change the IP to be in the same subnet as the monitored devices
+    #new_ip = input("Introduce la nueva direcciÃ³n IP: ")
+    if UDPconfig.get('Device',{}).get('New_config',True):
+        new_ip = UDPconfig.get('Device',{}).get('IP_Address')
+        netmask = UDPconfig.get('Device',{}).get('Netmask')
+        os.system(f'sudo ifconfig eth0 {new_ip} netmask {netmask} up')
+    
+def set_Netfilter():
+    
     #tshark filter
-    if port_list:
+    complete_filter = 'udp'
+    if UDPconfig.get('ports',{}).get('enable',True):
+        port_list = UDPconfig.get('ports',{}).get('Whitelist',[])
         ports_filter = 'or '.join([f'port {port} ' for port in port_list])
-    if src_ip_list:
-        src_ip_filter = 'or '.join([f'host {src_ip} ' for src_ip in src_ip_list])
-    if dst_ip_list:
-        dst_ip_filter = 'or '.join([f'host {dst_ip} ' for dst_ip in dst_ip_list])
+        complete_filter += f' and ({ports_filter})'
         
-    complete_filter = f'udp and ({ports_filter}) and ({src_ip_filter}) and ({dst_ip_filter})'
+    if UDPconfig.get('src_ips',{}).get('enable',True):
+        src_ip_list = UDPconfig.get('src_ips',{}).get('Whitelist',[])
+        src_ip_filter = 'or '.join([f'src host {src_ip} ' for src_ip in src_ip_list])
+        complete_filter += f' and ({src_ip_filter})'
+        
+    if UDPconfig.get('dst_ips',{}).get('enable',True):
+        dst_ip_list = UDPconfig.get('dst_ips',{}).get('Whitelist',[])
+        dst_ip_filter = 'or '.join([f'dst host {dst_ip} ' for dst_ip in dst_ip_list])
+        complete_filter += f' and ({dst_ip_filter})'
+    
+    #complete_filter = f'udp and ({ports_filter}) and ({src_ip_filter}) and ({dst_ip_filter})'
     print(complete_filter)
     return complete_filter
 
 def capture_packets():
     
     date_hour = datetime.now().strftime("%d%m%Y_%H%M%S")
-    OutFile = f'capture_{date_hour}.pcap'
-    Netfilter = set_Netfilter(ports,src_ips,dst_ips)
+    pcapFile = f'capture_{date_hour}.pcap'
+    csvFile = f'data_{date_hour}.csv'
+    
+    Netfilter = set_Netfilter()
     # tcpdump command to sniff UDP packets the specified port
     command = [
         'tshark',
         '-i', 'eth0',       # interfaz red a analizar (eth0)
         '-f', f'{Netfilter}',
-        '-w', f'{OutFile}',      # Guardar en un archivo pcap
+        '-w', f'{pcapFile}',      # Guardar en un archivo pcap
         '-T', 'fields',
         '-e', 'ip.src',
         '-e', 'ip.dst',
         '-e', 'udp.dstport',
-        #'-e', 'frame.len',
+        '-e', 'frame.time_epoch',
         '-e', 'data'
     ]
-   
+    
+    csvfile = open(csvFile, mode='w', newline = '')
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(['Timestamp', 'SeqCnt', 'COMID','RawFilterData','VarSizes','Variables']) # Encabezados csv
+    first_timestamp = None
+    
+    Filterconfig = Config_load('msgFilter.json')
+    Header_size = Filterconfig['config']['Header_size']
+    COM_ID_index = Filterconfig['config']['COM_ID_index']
+    COM_ID_size = Filterconfig['config']['COM_ID_size']
+    SeqCnt_index = Filterconfig['config']['SeqCnt_index']
+    SeqCnt_size = Filterconfig['config']['SeqCnt_size']
+    endian = Filterconfig['config']['endian']
+    msgFilter = Filterconfig['msgFilter']
+    
+    comIDs_whitelist = {msg['COM_ID']:msg['signals'] for msg in msgFilter} # conjunto de COM_IDs permitidos
+    
+    # Eliminar COM_ID 0 de los COM:IDs permitidos
+    if 0 in comIDs_whitelist:
+        del comIDs_whitelist[0]
+        
     print(f'Capturando paquetes UDP...(Ctrl+C para detener y guardar la captura)')
     
     try:
@@ -50,7 +90,7 @@ def capture_packets():
             
             output = process.stdout.readline() 
             if not output:
-                print('Sin entrada (Puede que tu filtro sea demasiado restrictivo o esté mal configurado)')
+                print('Sin entrada (Puede que tu filtro sea demasiado restrictivo o estÃ© mal configurado)')
                 break
             
             packetdata = output.decode('utf-8').strip()
@@ -58,42 +98,92 @@ def capture_packets():
                 #print(f'Packet received: {packetdata}')
                 fields = packetdata.split('\t')
                 #print(f'Packet splited: {fields}')
-                if len(fields) >= 3:
+                if len(fields) >= 4:
                     src_ip = fields[0]
                     dst_ip = fields[1]
                     dst_port = fields[2]
-                    #data_len = fields[3] # incluye header (solo nos importa el tamaño del payload)
-                    data_hex = fields[3] if len(fields) > 3 else ''
+                    timestamp = float(fields[3]) 
+                    data_hex = fields[4] if len(fields) > 4 else ''
                     
                     # Convertir contenido a bytes
                     data_bytes = bytes.fromhex(data_hex.replace(':',''))
                     data_list = list(data_bytes)
                     data_len = len(data_list)
-                    #print(f"Paquete capturado en puerto {dst_port} con IP de origen: {src_ip}, IP de destino: {dst_ip} y tamaño {data_len} bytes")
+                    
+                    if first_timestamp is None:
+                        first_timestamp = timestamp
+                    
+                    #print(f"Paquete capturado en puerto {dst_port} con IP de origen: {src_ip}, IP de destino: {dst_ip} y tamaÃ±o {data_len} bytes")
                     #print(f"Contenido: {data_list}")
                     #print('-' * 40)
+                    COMID = int.from_bytes(data_list[COM_ID_index:COM_ID_index+(COM_ID_size)],byteorder=endian)  # 'big' para big-endian y 'little' para little-endian
+                    #print(f'COMID: {COMID}')
+                    SeqCnt = int.from_bytes(data_list[SeqCnt_index:SeqCnt_index+(SeqCnt_size)],byteorder=endian)
+                    
+                    if COMID in comIDs_whitelist:
+                        print(f'COMID {COMID} in whitelist')
+                        timestamp_fix = timestamp - first_timestamp
+                        RawFilterData = []
+                        VarSizes = []
+                        Variables = []
+                        signals = comIDs_whitelist[COMID]
+                        
+                        for signal in signals:
+                            byte_index = signal['byte_index']
+                            #print(f'byte_index:{byte_index}')
+                            byte_index += Header_size
+                            #print(f'byte_index+Header:{byte_index}')
+                            size = signal['size']
+                            #print(f'size:{size}')
+                            if size>0:
+                                if byte_index + size <= len(data_list):
+                                    signal_bytes = data_list[byte_index:byte_index + size]
+                                    RawFilterData.extend(signal_bytes)
+                                    #print(f'RawFilterData:{RawFilterData}')
+                                    
+                                    if size == 1 or size == 2 or size == 4 or size == 8:
+                                        # Almacenar el tamaÃ±o de la variable
+                                        VarSizes.append(size)
+                                        # Convertir bytes a variable del tamaÃ±o determinado
+                                        signal_value = int.from_bytes(signal_bytes, byteorder =endian, signed=False)
+                                        # Almacenar el valor entero de la lista
+                                        Variables.append(signal_value)
+                                    else:
+                                        print(f'TamaÃ±o de variable no soportado: {size}')
+                                       
+                                        
+                                else:
+                                    print(f'byte_index out of reach (Header size + byte_index = {byte_index} > Data length = {len(data_list)})')
+                        
+                        # Convertir lista de enteros en cadena separada por comas
+                        VarSizes_str =  ','.join(map(str,VarSizes))
+                        Variables_str = ','.join(map(str,Variables))
+                        # Escribir en el archivo CSV
+                        csv_writer.writerow([timestamp_fix,SeqCnt,COMID,RawFilterData,VarSizes_str,Variables_str])
+                    else:
+                        #print(f'Rejected COMID: {COMID}')
+                        continue #omite el resto del bucle
+
+                    
                     
     except KeyboardInterrupt:
         
-        print("Captura detenida. Guardando datos...")
+        print("Captura detenida.Guardando datos...")
         process.terminate()
-        print(f"Datos guardados en {OutFile}")
+        print(f"Datos guardados en {pcapFile}")
         
     except Exception as e:
-        print(f"Ocurrió un error: {e}")
+        print(f"OcurriÃ³ un error: {e}")
         process.terminate()
+    
+    finally:
+        csvfile.close()
      
         
 if __name__ == "__main__":
     
-    #port = input("Introduce el puerto a capturar: ")
-    #src_ip_filter = input("Introduce la dirección IP de origen a filtrar (dejar en blanco para no filtrar): ")
-    new_ip = "192.168.1.100"
-    netmask = "255.255.255.0"
-    change_ip(new_ip,netmask)
-    
-    ports = [5007, 8888]
-    src_ips=["192.168.1.251"]
-    dst_ips=['224.1.1.1','224.1.3.3']
+    UDPconfig = Config_load('UDPconfig.json')
+
+    change_ip()
     
     capture_packets()
